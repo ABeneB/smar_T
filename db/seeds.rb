5bd68b70360584d3cba2a5bdd0572a6153c05006
Unknown <hello@neyou.de>
2017-03-28 22:23:41 +0200

    Changed user passwords
    Changed password for all users to "password"

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(username: "admin", email: "test@test.de", password: "testadmin", role: "admin")
-User.create(username: "planer", email: "planer@test.de", password: "testplaner", role: "planer")
-User.create(username: "driver", email: "driver@test.de", password: "testdriver", role: "driver")
-User.create(username: "user", email: "user@test.de", password: "testuser", role: "user")
+User.create!(username: "admin", email: "test@test.de", password: "password", role: "admin")
+User.create!(username: "planer", email: "planer@test.de", password: "password", role: "planer")
+User.create!(username: "driver", email: "driver@test.de", password: "password", role: "driver")
+User.create!(username: "user", email: "user@test.de", password: "password", role: "user")
