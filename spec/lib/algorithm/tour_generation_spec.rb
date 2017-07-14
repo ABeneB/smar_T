a750b80c6a21dae17a96177708d5dab934bb520e
Niels Petersen <np@magiclabs.de>
2017-07-14 15:51:27 +0200

    Add test for general preprocessing for heuristics and for Saving ++ init

---
+++ spec/lib/algorithm/tour_generation_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+require 'algorithm/tour_generation'
+
+RSpec.describe "TourGeneration" do
+
+  context ".preprocess" do
+    let!(:company) { FactoryGirl.create(:company)}
+    let!(:depot) { FactoryGirl.create(:depot, company: company, address: company.address) }
+    let!(:user_1) { FactoryGirl.create(:user_driver, password: "password", company: company) }
+    let!(:user_2) { FactoryGirl.create(:user_driver, password: "password", company: company) }
+    let!(:customer_1) { FactoryGirl.create(:customer, company: company) }
+    let!(:customer_2) { FactoryGirl.create(:customer, company: company) }
+    let!(:order_1) { FactoryGirl.create(:delivery_order, customer: customer_1) }
+    let!(:order_2) { FactoryGirl.create(:delivery_order, customer: customer_2, start_time: nil) }
+    let!(:order_3) { FactoryGirl.create(:delivery_order, customer: customer_1, start_time: DateTime.now + 24.hours) }
+    let!(:driver_1) { FactoryGirl.create(:active_driver, user: user_1) }
+    let!(:driver_2) { FactoryGirl.create(:inactive_driver, user: user_2) }
+    let!(:vehicle_1) { FactoryGirl.create(:vehicle, driver: driver_1, position: company.address) }
+    let!(:vehicle_2) { FactoryGirl.create(:vehicle, driver: driver_2, position: company.address) }
+    let(:preprocessed_output) { Algorithm::TourGeneration.preprocess(company.orders, company.drivers) }
+
+    it "returns only due orders" do
+      expect(preprocessed_output[0]).to include(order_1)
+      expect(preprocessed_output[0]).to include(order_2)
+      expect(preprocessed_output[0]).not_to include(order_3)
+    end
+
+    it "returns only active drivers" do
+      expect(preprocessed_output[1]).to include(driver_1)
+      expect(preprocessed_output[1]).not_to include(driver_2)
+    end
+  end
+end
--- spec/lib/algorithm/variants/savingsplusplus_spec.rb
+++ spec/lib/algorithm/variants/savingsplusplus_spec.rb
@@ -3,25 +3,45 @@ require 'algorithm/variants/savingsplusplus'
 
 RSpec.describe "SavingsPlusPlus Heuristic" do
 
-  context "initialisation phase" do
-      let!(:company) { FactoryGirl.create(:company)}
-      let!(:depot) { FactoryGirl.create(:depot, company: company, address: company.address) }
-      let!(:user) { FactoryGirl.create(:user_driver, password: "password", company: company)}
-      let!(:customer) { FactoryGirl.create(:customer, company: company) }
-      let!(:order) { FactoryGirl.create(:delivery_order, customer: customer) }
-      let!(:driver) { FactoryGirl.create(:active_driver, user: user) }
-      let!(:vehicle) { FactoryGirl.create(:vehicle, driver: driver, position: company.address) }
-      let(:day_tours) {
-        savingsplusplus = Algorithm::Variants::SavingsPlusPlus.new(company)
-        savingsplusplus.init(company.orders.to_a, company.drivers.to_a)
-      }
+  def order_in_tour?(tour, order)
+    for order_tour in tour.order_tours do
+      return true if order_tour.order == order
+    end
+    false
+  end
 
-    it "builds return one trivial" do
-      expect(day_tours.count()).to eq(1)
+
+  context "initialisation phase" do
+    let!(:company) { FactoryGirl.create(:company)}
+    let!(:depot) { FactoryGirl.create(:depot, company: company, address: company.address) }
+    let!(:user) { FactoryGirl.create(:user_driver, password: "password", company: company)}
+    let!(:customer_1) { FactoryGirl.create(:customer, company: company) }
+    let!(:customer_2) { FactoryGirl.create(:customer, company: company) }
+    let!(:order_1) { FactoryGirl.create(:delivery_order, customer: customer_1) }
+    let!(:order_2) { FactoryGirl.create(:delivery_order, customer: customer_2, start_time: nil) }
+    let!(:driver) { FactoryGirl.create(:active_driver, user: user) }
+    let!(:vehicle) { FactoryGirl.create(:vehicle, driver: driver, position: company.address) }
+    let(:day_tours) {
+      savingsplusplus = Algorithm::Variants::SavingsPlusPlus.new(company)
+      savingsplusplus.init(company.orders.to_a, company.drivers.to_a)
+    }
+
+    it "builds return trivial tour for every due order" do
+      expect(day_tours.count()).to eq(2)
+      for tour in day_tours do
+        expect(tour.order_tours.count).to eq(5)
+      end
     end
 
-    it "builds trivial tour with duration greater than 0" do
-      expect(day_tours[0].duration).to be > 0
+    it "builds day tours containing orders in right sequence" do
+      expect(order_in_tour?(day_tours[0], order_1)).to be(true)
+      expect(order_in_tour?(day_tours[1], order_2)).to be(true)
+    end
+
+    it "builds trivial tours with duration greater than 0" do
+      for tour in day_tours do
+        expect(tour.duration).to be > 0
+      end
     end
   end
 end
