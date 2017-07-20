b29ac248cbe3adacfdff7d244b19f576a279aa96
Niels Petersen <np@magiclabs.de>
2017-07-20 17:40:20 +0200

    Add and update test factories

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :delivery_order, class: Order do
     pickup_location { Faker::Address.street_name + " " + Faker::Address.building_number + " " + Faker::Address.postcode + " "  + Faker::Address.city }
-    capacity 3
+    capacity 10
     start_time { DateTime.now + 10.minutes }
+    end_time { DateTime.now + 2.hours }
     duration_delivery 20
     activ true
   end
