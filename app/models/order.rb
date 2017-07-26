cea50b6857a6fac380e33fa71b51de11a8ace478
Unknown <hello@neyou.de>
2017-07-26 17:19:39 +0200

    Catch unknown address in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,13 +7,17 @@ class Order < ActiveRecord::Base
   def geocode_locations
     if pickup_location
       coords = Geocoder.coordinates(self.pickup_location)
-      self.pickup_lat = coords[0]
-      self.pickup_long = coords[1]
+      unless coords.nil?
+        self.pickup_lat = coords[0]
+        self.pickup_long = coords[1]
+      end
     end
     if delivery_location
       coords = Geocoder.coordinates(self.delivery_location)
-      self.delivery_lat = coords[0]
-      self.delivery_long = coords[1]
+      unless coords.nil?
+        self.delivery_lat = coords[0]
+        self.delivery_long = coords[1]
+      end
     end
   end
 end
