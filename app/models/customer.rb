48234ded19232f8127f06f0557290b3309c7e5e5
Unknown <ceemon@gmx.net>
2017-08-22 17:02:26 +0200

    Translated all view attributes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,10 @@
 class Customer < ActiveRecord::Base
   belongs_to :company
   has_many :orders
-
+  validates :company, presence: {message: "Dieses Feld muss ausgefüllt werden"}
+  validates :name, presence: {message: "Dieses Feld muss ausgefüllt werden"}
   validates :priority, inclusion: { in: ["A", "B", "C", "D", "E"],
-  message: "%{value} ist nicht zulässig. Bitte benutzen Sie A,B,C, D oderr E" }
+  message: "Bitte benutzen Sie A,B,C, D oder E" }
 
   # Koordinaten aus Adresse
   geocoded_by :address
