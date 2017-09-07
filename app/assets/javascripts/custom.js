3cdc9110e57ce3043a1946f902ffdfe7825f6e7e
Unknown <hello@neyou.de>
2017-09-07 12:06:29 +0200

    Prompt page reload after change in order of tours to display the new map

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,12 +2,14 @@ $(function() {
   $("tbody.sortable").sortable({
     items: "tr:not(.unsortable)",
     update: function (event, ui) {
-      var data = $(this).sortable('serialize');
-      $.ajax({
-       data: data,
-       type: 'POST',
-       url: '/tours/positions/update'
-      });
+        var data = $(this).sortable('serialize');
+        $.ajax({
+            data: data,
+            type: 'POST',
+            url: '/tours/positions/update'
+        });
+        $("#map").hide();
+        $(".map-info").removeClass("hidden");
      }
   });
 });
