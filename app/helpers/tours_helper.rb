abf441bfa2d5ee63c705e780a22ed617df54f3cd
Nico Engler <hello@neyou.de>
2017-09-07 03:25:44 +0200

    Imporved views to display only relevant data in appropriate user formats

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -1,2 +1,16 @@
 module ToursHelper
+
+  def self.get_algorithm_display_text(algorithm)
+
+    if algorithm == AlgorithmEnum::M3PDP
+      display_text = "M3PDP"
+    elsif algorithm == AlgorithmEnum::M3PDPDELTA
+      display_text = "M3PDP Delta"
+    elsif algorithm == AlgorithmEnum::SAVINGSPP
+      display_text = "Savings++"
+    end
+
+    display_text
+  end
+
 end
