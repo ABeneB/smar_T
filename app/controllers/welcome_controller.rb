27b2b6b057f1c4b7f72e9f2b143c814e3933b9bf
Nico Engler <hello@neyou.de>
2017-09-21 16:50:23 +0200

    Added reference checking for vehicle in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,8 +6,10 @@ class WelcomeController < ApplicationController
       if current_user.company
         @drivers = current_user.company.drivers
         @hash = Gmaps4rails.build_markers(@drivers) do |driver, marker|
-          marker.lat driver.vehicle.latitude
-          marker.lng driver.vehicle.longitude
+          unless driver.vehicle.nil?
+            marker.lat driver.vehicle.latitude
+            marker.lng driver.vehicle.longitude
+          end
         end
       end
     end
