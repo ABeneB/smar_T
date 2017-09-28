15cbb32db21844a8662f9464c0e6991efdc624c1
Niels P <niels.p@live.de>
2017-09-28 19:26:18 +0200

    Add tab nav for switching between current  and completed tours

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -1,7 +1,6 @@
 module ToursHelper
 
-  def self.get_algorithm_display_text(algorithm)
-
+  def get_algorithm_display_text(algorithm)
     if algorithm == AlgorithmEnum::M3PDP
       display_text = "M3PDP"
     elsif algorithm == AlgorithmEnum::M3PDPDELTA
@@ -11,14 +10,23 @@ module ToursHelper
     end
 
     display_text
-
   end
 
-  def self.is_sortable_order_tour?(order_tour)
-
+  def is_sortable_order_tour?(order_tour)
     sortable_class = order_tour.kind != "delivery" ? false : true
     sortable_class
-
   end
 
+  def get_tour_status_as_string(status)
+    case status
+      when StatusEnum::GENERATED
+        t('helpers.tours.tour_generated')
+      when StatusEnum::APPROVED
+        t('helpers.tours.tour_approved')
+      when StatusEnum::STARTED
+        t('helpers.tours.tour_started')
+      when StatusEnum::COMPLETED
+        t('helpers.tours.tour_completed')
+    end
+  end
 end
