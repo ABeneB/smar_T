9a24b31cb77d7999dae59dae8a161c60b93c8b13
Unknown <ceemon@gmx.net>
2017-10-08 16:19:49 +0200

    Added validation of 1 to 1 relation betw. vehicle and driver

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,6 +2,8 @@ class Vehicle < ActiveRecord::Base
   belongs_to :company
   belongs_to :driver
 
+  validates :driver_id, uniqueness: true, :allow_nil => true
+
   # Koordinaten aus Adresse
   geocoded_by :position
   after_validation :geocode
