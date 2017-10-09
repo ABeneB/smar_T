3e1ccbddacdd8dea57613445b6bcbb542015ebb7
Unknown <ceemon@gmx.net>
2017-10-09 18:55:29 +0200

    Added validation to depot
    Made address mandatory and duration must be 0 or geater

--- app/models/depot.rb
+++ app/models/depot.rb
@@ -1,5 +1,8 @@
 class Depot < ActiveRecord::Base
   belongs_to :company
+   
+  validates :address, presence: true
+  validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
   # Koordinaten aus Adresse
   geocoded_by :address
