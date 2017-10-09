0332c37a70cc23538a632532ba521942a000761e
Nico Engler <hello@neyou.de>
2017-10-09 13:16:20 +0200

    Added functionality to ensure the superadmin loses the company reference

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < Devise::SessionsController
   after_filter :set_google_maps_api_key, :only => :create
+  before_filter :unassign_superadmin, :only => :create
 
   def set_google_maps_api_key
     Geocoder.configure(api_key: current_user.try(:company).try(:google_maps_api_key))
+  end
+
+  def unassign_superadmin
+    user = User.where(role: "superadmin").first
+    user.company = nil
+    user.save
   end
 end
