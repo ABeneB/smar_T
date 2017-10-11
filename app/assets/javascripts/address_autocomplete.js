ab6f27511ea7b41a566f93111f4c885ed5d7bde7
Unknown <ceemon@gmx.net>
2017-10-11 14:20:22 +0200

    Added second jquery function to handle two autocompleted addressfields

--- app/assets/javascripts/address_autocomplete.js
+++ app/assets/javascripts/address_autocomplete.js
@@ -1,6 +1,8 @@
 //= require typeahead.bundle
 //= require typeahead-addresspicker
 
+/*Address autocompletion for all 'address' fields and 'order_pickup_location' field*/
+
 $( document ).ready(function() {
 
     var addressPicker = new AddressPicker({
@@ -31,5 +33,40 @@ $( document ).ready(function() {
 
     $('#address').on('blur', function (event, result) {
         $('#address').val(address);
+    });
+});
+
+/*Address autocompletion for 'order_delivery_location' field*/
+
+$( document ).ready(function() {
+
+    var addressPicker = new AddressPicker({
+            language: 'de'
+        }
+    );
+
+    $('#order_delivery_location').typeahead(
+        {
+            highlight: true
+        },
+        {
+            displayKey: 'description',
+            source: addressPicker.ttAdapter(),
+            templates: {
+                empty: [
+                    '<div class="empty-message">Keine Adresse gefunden</div>'].join('\n')
+            }
+        }
+    );
+
+    var address = "";
+    addressPicker.bindDefaultTypeaheadEvent($('#order_delivery_location'));
+    $(addressPicker).on('addresspicker:selected', function (event, result) {
+        address = result.placeResult.formatted_address;
+        $('#order_delivery_location').val(address);
+    });
+
+    $('#order_delivery_location').on('blur', function (event, result) {
+        $('#order_delivery_location').val(address);
     });
 });
