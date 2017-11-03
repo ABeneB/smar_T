148f57e197607ac01e3f5036d0f14d532411c1af
Unknown <ceemon@gmx.net>
2017-11-03 16:21:14 +0100

    Allow blank registration number for vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,7 +3,7 @@ class Vehicle < ActiveRecord::Base
   belongs_to :driver
 
   validates :driver_id, uniqueness: true, :allow_nil => true
-  validates :registration_number, uniqueness: true, :allow_nil => true
+  validates :registration_number, uniqueness: true, :allow_nil => true, :allow_blank => true
   validates :position, presence: true
 
   # Koordinaten aus Adresse
