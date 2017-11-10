024783c515f886b8064a5d8cc85d456364011b14
Niels Petersen <niels.p@live.de>
2017-11-10 15:08:12 +0100

    Ensure sanitization of filename for order import

--- app/controllers/order_import_controller.rb
+++ app/controllers/order_import_controller.rb
@@ -6,33 +6,39 @@ class OrderImportController < ApplicationController
 
   def confirm
     import_file = params[:file]
-    new_order_ids = []
     if import_file
-      CSV.foreach(import_file.path, col_sep: ";", encoding: 'UTF-8') do |row|
-        if is_number?(row[0].try(:squish)) # ignore header and validate customer_reference
-          order = Order.new
-          order.customer = Customer.customer_by_customer_reference(row[0].try(:squish).try(:to_i), current_user.company, row[1].try(:squish), row[3].try(:squish))
-          order.location = row[2].try(:squish)
-          order.duration = row[4].try(:squish)
-          order.capacity = 0
-          order.order_ref = row[5].try(:squish)
-          order.status = OrderStatusEnum::INVALID
-          order.comment = ""
-          order.comment2 = ""
-          order.save!
-          new_order_ids.push(order.id)
+      new_order_ids = []
+      sanitize_filename(import_file.original_filename)
+      if import_file
+        CSV.foreach(import_file.path, col_sep: ";", encoding: 'UTF-8') do |row|
+          if is_number?(row[0].try(:squish)) # ignore header and validate customer_reference
+            order = Order.new
+            order.customer = Customer.customer_by_customer_reference(row[0].try(:squish).try(:to_i), current_user.company, row[1].try(:squish), row[3].try(:squish))
+            order.location = row[2].try(:squish)
+            order.duration = row[4].try(:squish)
+            order.capacity = 0
+            order.order_ref = row[5].try(:squish)
+            order.status = OrderStatusEnum::INVALID
+            order.comment = ""
+            order.comment2 = ""
+            order.save!
+            new_order_ids.push(order.id)
+          end
         end
       end
-    end
-    @check_orders = Order.where(id: new_order_ids)
-    @check_orders.each do |order|
-      if order.lat && order.long
-      order.status = OrderStatusEnum::ACTIVE
-      order.save!
+      @check_orders = Order.where(id: new_order_ids)
+      @check_orders.each do |order|
+        if order.lat && order.long
+        order.status = OrderStatusEnum::ACTIVE
+        order.save!
+        end
       end
+      @imported_orders = Order.where(id: new_order_ids).where.not(status: 4)
+      @invalid_orders = Order.where(id: new_order_ids).where(status: 4)
+    else
+      flash[:error] = t('.no_file_provided')
+      redirect_to action: :file
     end
-    @imported_orders = Order.where(id: new_order_ids).where.not(status: 4)
-    @invalid_orders = Order.where(id: new_order_ids).where(status: 4)
   end
 
   def complete
@@ -50,5 +56,4 @@ class OrderImportController < ApplicationController
     def order_import_params
       params.require(:order_import).permit(:file, :orders)
     end
-
 end
--- app/helpers/order_import_helper.rb
+++ app/helpers/order_import_helper.rb
@@ -3,4 +3,14 @@ module OrderImportHelper
   def is_number? string
     true if Float(string) rescue false
   end
+
+  def sanitize_filename(filename)
+    filename.strip.tap do |name|
+      # get only the filename, not the whole path
+      name.sub! /\A.*(\\|\/)/, ''
+      # Finally, replace all non alphanumeric, underscore
+      # or periods with underscore
+      name.gsub! /[^\w\.\-]/, '_'
+    end
+  end
 end
